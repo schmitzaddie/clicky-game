c5ea7a41279e0f777be5c285e455f2a463133385
schmitzaddie <schmitzaddie@gmail.com>
2019-04-15 13:09:04 -0500

    Updates

---
+++ precache-manifest.e04b7f1720d105b38553524dde414a88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5ca730974c340031976",
+    "url": "/clicky-game/static/css/main.3fec8472.chunk.css"
+  },
+  {
+    "revision": "d5ca730974c340031976",
+    "url": "/clicky-game/static/js/main.f5378536.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "3085381d82e81e6326b2",
+    "url": "/clicky-game/static/js/2.4c9fe737.chunk.js"
+  },
+  {
+    "revision": "744869476449cd4462136e8cb1d8254f",
+    "url": "/clicky-game/index.html"
+  }
+];
